d7e186ab7b068e1cd21d225aee3b6dbb5afaffc0
rob shaw <rob@borwahs.com>
2019-04-28 23:54:49 -0400

    Configure Express and launch on start.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Twitter Bot Wallet Dropper');
+/** Third Party **/
+const express = require('express');
+
+/** Setup */
+const server = express();
+
+/** Constants */
+const DEFAULT_PORT = 3000;
+
+/** Handlers **/
+function serverStartHandler() {
+    console.log(`Wallet Dropper Web Service Listening on ${DEFAULT_PORT}`);
+}
+
+server.listen(DEFAULT_PORT, serverStartHandler);
