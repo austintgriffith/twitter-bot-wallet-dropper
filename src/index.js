9c0eecb93f002d04f1c3202c28cd7dcb9547eee6
rob shaw <rob@borwahs.com>
2019-04-29 00:09:47 -0400

    Explicitly capture uncaught exceptions and log to console.

--- src/index.js
+++ src/index.js
@@ -12,8 +12,17 @@ function serverStartHandler() {
     console.log(`Wallet Dropper Web Service Listening on ${DEFAULT_PORT}`);
 }
 
+function uncaughtExceptionHandler(err) {
+  console.error('-------------------------------');
+  console.error('-----EXCEPTION ENCOUNTERED-----');
+  console.error(err);
+  console.error('-------------------------------');
+}
+
 server.listen(DEFAULT_PORT, serverStartHandler);
 
 server.get('*', function(req, res){
   res.send({ status: 404, message: 'Not Found'}, 404);
 });
+
+process.on('uncaughtException', uncaughtExceptionHandler);
